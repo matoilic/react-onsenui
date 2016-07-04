49e684c31b2e958a9cd07bac4fcedbf284b58a52
Patrick Klitzke <philologos14@gmail.com>
2016-07-04 14:40:50 +0900

    mockup until error is invalid state

--- demo/ssr.js
+++ demo/ssr.js
@@ -2,11 +2,37 @@ global.navigator = {
   userAgent: 'test'
 };
 
+global.window = {
+  addEventListener: () => {},
+  Event: {},
+  getComputedStyle: () => []
+};
+
+global.self = {
+};
+
+global.document = {
+  createElement: () => {},
+  createElementNS: () => {},
+};
+
+
+
+global.Node = {
+  prototype: {}
+};
+
+global.Element = {
+    prototype: {}
+};
+
+
 console.log(navigator);
 
 import {Page} from '../src/index.js';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import '../OnsenUI/build/js/onsenui.js';
+//import '../OnsenUI/build/js/onsenui.js';
+require('onsenui');
 
 ReactDOMServer.renderToString(<Page />);
